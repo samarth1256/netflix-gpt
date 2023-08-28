d1a866966c585a9e7b8620cd5e68f0037239ab9a
samarth1256 <samarthdua1256@gmail.com>
2023-08-28 15:43:57 +0530

    validation && redux && auth

--- README.md
+++ README.md
@@ -2,7 +2,19 @@ Netflix-gpt
 
 1. create-react-app
 2. configured tailwind
-3. Login form
+3. Header
+3. Routing of app
+4. Login form
+5. Sign up form
+6. Form validation
+7. useRef hook
+8. Firebase setup
+9. Deploying our app to production
+10. Create Signup user account
+11. Implement sign in api from firebase
+12. Created redux store with userSlice
+13. Implement Sign out
+14. Update profile
 
 #Features
 -login/sign up    
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,74 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
+import { checkValidData } from '../utils/validate';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {auth} from '../utils/firebase'
+import { useNavigate } from 'react-router-dom';
+import { useDispatch} from 'react-redux';
+import { addUser } from '../utils/userSlice';
+
 
 const Login = () => {
+
+  const navigate=useNavigate();
+  const dispatch=useDispatch();
+
     const [isSignInForm,setIsSignInForm]=useState(true);
+    const [errorMessage,setErrorMessage]=useState(null);
+    const name=useRef(null);
+    const email=useRef(null);
+    const password=useRef(null);
+
+    const handleButtonClick=()=>{
+        const message=checkValidData(email.current.value,password.current.value)
+        setErrorMessage(message);
+        if(message) return;
+
+        if(!isSignInForm){
+            createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+updateProfile(user, {
+  displayName: name.current.value, photoURL: "https://occ-0-2085-2186.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABY5cwIbM7shRfcXmfQg98cqMqiZZ8sReZnj4y_keCAHeXmG_SoqLD8SXYistPtesdqIjcsGE-tHO8RR92n7NyxZpqcFS80YfbRFz.png?r=229"
+}).then(() => {
+  // Profile updated!
+  // ...
+  const {uid,email,displayName,photoURL} = auth.currentUser;
+          dispatch(addUser({uid:uid,email:email,displayName:displayName,photoURL:photoURL}))
+    navigate("/browser")
+}).catch((error) => {
+  setErrorMessage(error.message)
+});
+    
+    
+    
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    setErrorMessage(errorCode+"-"+errorMessage)
+    // ..
+  });
+
+        }else{
+            signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user)
+    navigate("/browse")
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    setErrorMessage(errorCode+"-"+errorMessage)
+  });
+
+        }
+    }
 
     const toggleSignInForm=()=>{
         setIsSignInForm(!isSignInForm);
@@ -14,13 +80,14 @@ const Login = () => {
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/64774cd8-5c3a-4823-a0bb-1610d6971bd4/IN-en-20230821-popsignuptwoweeks-perspective_alpha_website_large.jpg"
         alt="logo"/>
       </div>
-      <form className='absolute p-12 bg-black w-4/12 my-28 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
+      <form onSubmit={(e)=>e.preventDefault()} className='absolute p-12 bg-black w-4/12 my-28 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
       <h1 className='font-bold text-3xl py-4'>{isSignInForm? "Sign In":"Sign Up"}</h1>
-      {!isSignInForm && (<input type='text' placeholder='Full Name' className='p-4 my-4 w-full bg-gray-700'></input>)}
-        <input type='text' placeholder='Email Address' className='p-4 my-4 w-full bg-gray-700'></input>
-        <input type='password' placeholder='Password' className='p-4 my-4 w-full bg-gray-700'></input>
-        {!isSignInForm&&(<input type='password' placeholder='Confirm Password' className='p-4 my-4 w-full bg-gray-700'></input>)}
-        <button className='p-4 my-4 w-full bg-red-700 rounded-lg'>{isSignInForm? "Sign In":"Sign Up"}</button>
+      {!isSignInForm && (<input ref={name} type='text' placeholder='Full Name' className='p-4 my-4 w-full bg-gray-700'></input>)}
+        <input ref={email} type='text' placeholder='Email Address' className='p-4 my-4 w-full bg-gray-700'></input>
+        <input ref={password} type='password' placeholder='Password' className='p-4 my-4 w-full bg-gray-700'></input>
+    
+        <p className='text-red-500 font-bold text-lg py-2'>{errorMessage}</p> 
+        <button className='p-4 my-4 w-full bg-red-700 rounded-lg' onClick={handleButtonClick}>{isSignInForm? "Sign In":"Sign Up"}</button>
         <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm? "New to Netflix, Sign up now":"Already registered? Sign In Now."}</p>
       </form>
     </div>
